3cdd2ace1fede802bca4b9bbff6196907c425004
yazawa8 <yaza_ta@yahoo.co.jp>
2024-04-07 15:08:43 +0900

    gamesプロジェクトの作成

---
+++ src/jp/co/kiramex/polymorphism/Animal.java
@@ -0,0 +1,5 @@
+package jp.co.kiramex.polymorphism;
+
+public abstract class Animal {
+    public abstract void barking();
+}
---
+++ src/jp/co/kiramex/polymorphism/Cat.java
@@ -0,0 +1,10 @@
+package jp.co.kiramex.polymorphism;
+
+public class Cat extends Animal {
+
+    @Override
+    public void barking() {
+        System.out.println("猫は、ニャーと鳴く");
+    }
+
+}
